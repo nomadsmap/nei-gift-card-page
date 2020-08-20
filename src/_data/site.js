a02836e8b8d53bed1f8beaed567c3b657bed989b
Damon <51332036+nomadsmap@users.noreply.github.com>
2020-08-20 12:15:03 -0400

    update meta

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,11 +1,5 @@
 module.exports = {
-  "title": "Web Starter",
-  "description": "Minimal boilerplate for new projects built with Eleventy, Tailwind, PurgeCSS and PostCSS",
-  "url": "https://webstarter.chriscollins.me",
-  "author": "Chris Collins",
-  "meta_data": {
-    "twitter": "@scottishstoater",
-    "default_social_image": "/static/default_social_image.jpg"
-  },
+  "title": "NEI and Microdrones Voucher",
+  "description": "Save on Drones for Surveying with NEI and Microdrones",
   "env": process.env.ELEVENTY_ENV === 'production'
 }
